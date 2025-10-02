f75af9a48413d74e8f4b5178019216d760b0697d
sanjeewa-dev <svk6061@gmail.com>
2025-10-02 21:51:28 +0530

    add modified hero sec

---
+++ client/src/Pages/Home/Components/HeroSection/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeroSection() {
+  return (
+    <div className='Hero-section'>
+      <div className='hero-section-text'>
+        <div className='hero-section-heading'></div>
+      </div>
+      <div className='hero-section-logo-image'></div>
+    </div>
+  )
+}
